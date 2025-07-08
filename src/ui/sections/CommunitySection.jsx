3dbe8f9aeebb8a0edc0bbe081d18d3f4a665a5e7
ArbishMunawar <167678183+ArbishMunawar@users.noreply.github.com>
2025-07-07 23:18:42 -0700

    Update CommunitySection.jsx

--- src/ui/sections/CommunitySection.jsx
+++ src/ui/sections/CommunitySection.jsx
@@ -8,7 +8,7 @@ import Wicon from '../../assets/icons/Wicon'
 const CommunitySection = () => {
   return (
    <div className="bg-gradient-to-r from-[#FBEEEE] to-[#F7FFFE] py-[67px] ">
-   <div className='px-5 lg:max-w-[1200px] mx-auto md:flex md:justify-between md:items-center lg:justify-between gap-44'>
+   <div className='px-5 md:px-0 lg:max-w-[1200px] mx-auto md:flex md:justify-between md:items-center lg:justify-between gap-44'>
     <div className='py-5'>
         <h2 className='font-bold text-xl lg:text-2xl pb-4'>Community</h2>
         <p>Join the Storyious community on social media! Connect with fellow readers and writers, share your favorite stories, and stay updated on the latest tales, writing tips, and storytelling events.</p>
