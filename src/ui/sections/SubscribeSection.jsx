05f991fd0033f507196fd28f49678e17ab5446b1
ArbishMunawar <167678183+ArbishMunawar@users.noreply.github.com>
2025-07-15 23:40:56 -0700

    Howtests

--- src/ui/sections/SubscribeSection.jsx
+++ src/ui/sections/SubscribeSection.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const SubscribeSection = () => {
   return (
-    <div className="bg-gradient-to-r from-[#fef6f6] to-[#f0fdfd] rounded-0 lg:rounded-xl p-6 sm:p-10 my-[50px] lg:my-[80px]  max-w-5xl mx-auto text-center shadow-sm">
-      <h2 className="text-xl sm:text-2xl font-semibold text-rasin-black mb-2">
+    <div className="bg-gradient-to-r from-[#fef6f6] to-[#f0fdfd] rounded-0 lg:rounded-xl py-[48px] lg:py-[58px]  my-[50px] lg:my-[80px]  max-w-5xl mx-auto text-center shadow-sm px-[20px] lg:px-0">
+      <h2 className="text-medium font-[700] text-rasin-black pb-[20px]">
         Subscribe to Get Updated!
       </h2>
-      <p className="text-sm sm:text-base text-text-gray max-w-2xl mx-auto mb-6">
+      <p className="text-extra-small font-[400] text-text-gray max-w-2xl mx-auto mb-6">
         Filter out the noise and enrich your inbox with guidance that empowers
         your CSS and PMS preparation. Stay informed with expert advice, curated
         resources, and strategies tailored to your success!
       </p>
       <div className="md:flex md:items-center md:justify-center ">
-      <form className="flex  sm:flex-row  md:w-[50%] xl:w-[40%] md:flex md:items-center md:justify-center items-center border rounded-md bg-white border-light-gray justify-center ">
+      <form className="flex  sm:flex-row  md:w-[50%] lg:w-[40%] xl:w-[40%] md:flex md:items-center md:justify-center items-center border rounded-md bg-white border-light-gray justify-center ">
         <input
           type="email"
           placeholder="Write your email"
@@ -22,7 +22,6 @@ const SubscribeSection = () => {
           type="submit"
           className="bg-azure-blue hover:bg-azure-blue text-white px-6 py-2 rounded-md transition duration-300"
         >
-          
           Subscribe
         </button>
       </form>
